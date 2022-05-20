5daad75a84f758fe012e8af51248548e6b8dd531
Nick(Seungyun Kim) <sy5072@naver.com>
2022-05-20 14:53:42 +0900

    Strings

---
+++ String.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+let favoriteBook = "The Sun Also Rises"
+
+let valueOfStringNum = "1234"
+
+let hanguker = "안녕"
+
+let imojihaha = "😂"
+
+let sayHello = hanguker + " " + imojihaha
+
+let name = "Kim"
+
+let age = 23
+
+// String Interpolation
+let sayHelloToSomeone = "\(hanguker), \(name) \(age)"
+ 
+let sayHi = "\"Hi\"" 
